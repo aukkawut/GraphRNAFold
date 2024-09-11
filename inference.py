8bfbc59c4d194a49655ac910faf60fa8792f53d8
Aukkawut Ammartayakun <50354662+aukkawut@users.noreply.github.com>
2024-09-10 20:43:43 -0400

    Update inference.py

--- inference.py
+++ inference.py
@@ -6,11 +6,11 @@ import numpy as np
 from arnie.bpps import bpps
 
 
-def GCNfold_prediction(rna_seq, GCNfold, one_type=True, threshold=0.35):
+def GraphRNAFold_prediction(rna_seq, GraphRNAFold, one_type=True, threshold=0.35):
     edge_feature = bpps(rna_seq, "vienna_2")
     node_feature = extract_rna_node_features(rna_seq)
     mask = possible_pairs(rna_seq)
-    out = GCNfold.predict([node_feature[np.newaxis,...], edge_feature[np.newaxis,...], mask[np.newaxis, :, :, np.newaxis]])
+    out = GraphRNAFold.predict([node_feature[np.newaxis,...], edge_feature[np.newaxis,...], mask[np.newaxis, :, :, np.newaxis]])
     map_pred = np.squeeze(conditional_argmax_pp(out, threshold=threshold)[0], axis=-1)
     
     if one_type:
@@ -20,11 +20,11 @@ def GCNfold_prediction(rna_seq, GCNfold, one_type=True, threshold=0.35):
     return dot_pred
 
 model_path = './model/GCNfold-V.h5'
-GCNfold = keras.models.load_model(model_path, custom_objects={"GCSConv":spektral.layers.GCSConv, 
+GraphRNAFold = keras.models.load_model(model_path, custom_objects={"GCSConv":spektral.layers.GCSConv, 
                                                               "weighted_BCE_loss":weighted_BCE_loss, "F1_score": F1_score})
 
 # example use case!
 rna_seq = 'CACACAAGGCAGAUGGGCUAUAUAAACGUUUUCGCUUUUCCGUUUACGAUAUAUAGUCUACUCUUGUGCAGAAUGAAUUCUCGUAACUACAUAGCACAAGUAGAUGUAGUUAACU'
 
-predict_structure = GCNfold_prediction(rna_seq, GCNfold, threshold=0.35)
+predict_structure = GraphRNAFold_prediction(rna_seq, GCNfold, threshold=0.35)
 print(predict_structure)
